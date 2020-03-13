f730b1062f89b72c3abfe3de51bacdcee6f8fa07
Kasia <kasia@miroartdesign.com>
2020-03-13 16:13:44 -0400

    test

--- src/app.js
+++ src/app.js
@@ -26,6 +26,7 @@ app.get('/', (req, res) => {
     res.send('hello from green-thumbs-up!')
 });
 
+
 app.use(function errorHandler(error, req, res, next) {
     let response;
     if (NODE_ENV === 'production') {
