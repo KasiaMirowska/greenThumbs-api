1d04b9dcd4db91e48e5c9b35e277f906b4a62fb3
Kasia <kasia@miroartdesign.com>
2020-03-20 22:10:57 -0400

    deplying

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL:process.env.DB_URL || 'postgresql://kamirska@localhost/green-thumb-data',
-    TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://kamirska@localhost/green-thumb-test',
+    DATABASE_URL:process.env.DATABASE_URL || 'postgresql://kamirska@localhost/green-thumb-data',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://kamirska@localhost/green-thumb-test',
     JWT_SECRET: process.env.JWT_SECRET || 'zielony-sekret',
+    CLIENT_ORIGIN: 'https://green-thumbs-up.now.sh/'
 }
