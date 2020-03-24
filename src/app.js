4546469e6044613d5f913d73b361a7d12ed6b216
Kasia <kasia@miroartdesign.com>
2020-03-23 23:29:32 -0400

    deploy

--- src/app.js
+++ src/app.js
@@ -26,12 +26,12 @@ app.use(reviewsRouter);
 app.get('/', (req, res) => {
     res.send('hello from green-thumbs-up!')
 });
-app.use(function(req, res, next) {
-    // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Origin", "https://green-thumbs-up.now.sh/");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function(req, res, next) {
+//     // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//     res.header("Access-Control-Allow-Origin", "https://green-thumbs-up.now.sh/");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
 app.use(function errorHandler(error, req, res, next) {
     let response;
--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ module.exports = {
     DATABASE_URL:process.env.DATABASE_URL || 'postgresql://kamirska@localhost/green-thumb-data',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://kamirska@localhost/green-thumb-test',
     JWT_SECRET: process.env.JWT_SECRET || 'zielony-sekret',
-    CLIENT_ORIGIN: 'https://green-thumbs-up.now.sh/'
+    CLIENT_ORIGIN: 'https://green-thumbs-up.now.sh'
 }
